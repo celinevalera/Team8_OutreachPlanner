e984be707cde01f1fdc8f6d5a1895a2bd0cb0abd
eriesegui <qemcsegui@tip.edu.ph>
2022-10-17 01:17:17 +0800

    Cleaning Repo

--- outreach_planner/users/forms.py
+++ outreach_planner/users/forms.py
@@ -10,7 +10,6 @@ class RegisterUserForm(UserCreationForm):
     last_name=forms.CharField(max_length=50)
     email= forms.EmailField()
     
-
     class Meta: 
         model = User
         fields = ('username','first_name','last_name','email', 'password1','password2')
