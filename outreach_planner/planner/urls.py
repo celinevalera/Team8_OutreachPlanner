8ebb70a2254c9cbc9c670982d5bc8ee75cd482d9
celinevalera <qmcjdvalera@tip.edu.ph>
2022-10-17 00:13:35 +0800

    Changed Repo

--- outreach_planner/planner/urls.py
+++ outreach_planner/planner/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('list_venue', views.list_venue, name='list-venue'),
     path('show_venue/<venue_id>', views.show_venue, name='show-venue'),
     path('search_venue', views.search_venue, name='search-venue'),
-<<<<<<< HEAD
     path('inbox/',views.inbox, name="inbox")
-=======
     path('update_venue/<venue_id>', views.update_venue, name='update-venue'),
->>>>>>> test_merge
 ]
