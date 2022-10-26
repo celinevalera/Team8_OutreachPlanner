e14cb74eb90e7961713fa69941241bd3b17e3bc7
eriesegui <qemcsegui@tip.edu.ph>
2022-10-26 11:13:02 +0800

    Cleaning repo

--- outreach_planner/planner/urls.py
+++ outreach_planner/planner/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('list_venue', views.list_venue, name='list-venue'),
     path('show_venue/<venue_id>', views.show_venue, name='show-venue'),
     path('search_venue', views.search_venue, name='search-venue'),
-    path('inbox/',views.inbox, name="inbox"),
+    path('inbox',views.inbox, name="inbox"),
     path('update_venue/<venue_id>', views.update_venue, name='update-venue'),
     path('calendar/',views.calendar),
 ]
