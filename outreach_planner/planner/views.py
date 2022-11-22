8f4342f820e13847fec53f7f0ce0b54b91d7718c
SirChubbx17 <qjccdelacruz@gmail.com>
2022-11-22 22:45:01 +0800

    FIXED

--- outreach_planner/planner/views.py
+++ outreach_planner/planner/views.py
@@ -159,18 +159,3 @@ def event_pdf(request, event_id):
     if pisa_status.err:
        return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
-
-@csrf_exempt
-def update(request):
-    if request.method == "POST":
-        '''
-        pass the path of the directory where your project will be
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-        repo = git.Repo("test.pythonanywhere.com/")
-        origin = repo.remotes.origin
-        origin.pull()
-        return HttpResponse("Updated code on PythonAnywhere")
-    else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
